a0beee5a2980c234e2831b3fb6e86a889bf74635
Michael Holzbach <maholzbach@gmail.com>
2023-07-28 10:19:43 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/path", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+app.post("/all-boats", (req, res, next) => {
+  return;
+});
+
+module.exports.handler = serverless(app);
